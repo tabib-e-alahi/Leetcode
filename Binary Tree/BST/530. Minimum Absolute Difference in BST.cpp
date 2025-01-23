109b4338168d119158762c227fd7534080376cbe
tabib-e-alahi <etabib.alahi@gmail.com>
2025-01-23 12:09:41 +0600

    leetcode bst 2 problem added

---
+++ Binary Tree/BST/530. Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> v;
+    void convert_to_vector(TreeNode* root) {
+        if (root == NULL)
+            return;
+        convert_to_vector(root->left);
+        if(root)    
+            v.push_back(root->val);    
+        convert_to_vector(root->right);
+    }
+    int getMinimumDifference(TreeNode* root) {
+        convert_to_vector(root);
+        int min_diff = INT_MAX;
+        for(int i = 1; i < v.size(); i++){
+            min_diff = min(min_diff, (v[i] -v[i-1]));
+        }
+        return min_diff;
+    }
+};
